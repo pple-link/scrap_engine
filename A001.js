9ff164dcaef70797b30336e93d53e2d6bbd0bf2e
Q00 <jqyu.lee@gmail.com>
2019-08-19 06:18:13 +0900

    fix: 🔥 add sleep

--- A001.js
+++ A001.js
@@ -1,14 +1,16 @@
 const puppeteer = require('puppeteer');
 
+
 (async () => {
+    const timeout = ms => new Promise(res => setTimeout(res, ms))
     const browser = await puppeteer.launch({headless : false})
     const page = await browser.newPage()
     await page.goto('https://gall.dcinside.com/mgallery/board/write/?id=blooddonation')
 
     const sid = '피플'
     const spw = 'people123'
-    const subject = 'test'
-    const content = 'content'
+    const subject = '질문이 있습니다!'
+    const content = '안녕하세요 지정헌혈이 필요한 사람들이 이용할 수 있는 자동헌혈 게시판 플랫폼이 있으면 어떨 것 같나요? 글을 보고 헌혈해주실 사람이 많을까요'
     await page.evaluate((id, pw, sub) => {
         document.querySelector('#name').value = id
         document.querySelector('#password').value = pw
@@ -25,9 +27,11 @@ const puppeteer = require('puppeteer');
 
         const iframeP = iframeDoc.querySelector('.tx-content-container');
 
-        iframeP.innerHTML = '123123'
+        iframeP.innerHTML = cont 
     }, content);
+    await timeout(3000)
     await page.click('[data-no="0"]')
+    await timeout(3000)
     await page.click('button[class="btn_blue btn_svc write"]')
     await page.waitForNavigation()
 
