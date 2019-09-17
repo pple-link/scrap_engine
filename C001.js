3ceff77c67dc6f4dc77f3372aa4c5405dde42fe0
eurobin4321 <eurobin4321@gmail.com>
2019-09-17 19:42:08 +0900

    fix: 🔥 naver-cafe auth fail fix

--- C001.js
+++ C001.js
@@ -4,18 +4,19 @@ const puppeteer = require('puppeteer');
 var request = require('request');
 
 // 관리자 아이디와 토큰
-var client_id = 'KvHy4Dic89O1peaeRkg_';
-var client_secret = 'ot0gOvhgm7';
+var client_id = '8QRsT_jCVZit1EFLbaUx';
+var client_secret = 'J48efEqkfA';
 var state = "200";
 var redirectURI = encodeURI("http://127.0.0.1:3000");
 var api_url = 'https://nid.naver.com/oauth2.0/authorize?response_type=code&client_id=' + client_id + '&redirect_uri=' + redirectURI + '&state=' + state;
+
+
 
 app.get('/',function(req, res){
 	
 	res.writeHead(200, {'Content-Type': 'text/html;charset=utf-8'});
 	const code = req.query.code;
 	const state = req.query.state;
-	console.log(req.query);
 
 	var login_options = {
 		url: 'https://nid.naver.com/oauth2.0/token?grant_type=authorization_code&client_id='+ client_id + '&client_secret=' + client_secret + '&redirect_uri=' + redirectURI + '&code=' + code + '&state=' + state,
@@ -37,31 +38,31 @@ app.get('/',function(req, res){
 			console.log('토큰 요청 후 응답 : ', body_data);
 
 			// 카페 고유값 및 게시판
-			const clubid = "29872001";// 카페의 고유 ID값
-			const menuid = "1";
+			const clubid = "10577860";// 카페의 고유 ID값
+			const menuid = "2";
+
+			const cafe_list = [
+				{clubid: "29872737", menuid: "1"}, // 피플 공식 카페
+				{clubid: "10577860", menuid: "2"}, // 고민상담 카페
+			];
 
 			// 카페 게시판 주소
-			const cafe_api_url = 'https://openapi.naver.com/v1/cafe/' + clubid + '/menu/' + menuid + '/articles';
+			const cafe_api_url = 'https://openapi.naver.com/v1/cafe/' + cafe_list[0].clubid + '/menu/' + cafe_list[0].menuid + '/articles';
 
 			console.log("게시하는 주소 : ", cafe_api_url);
 
-			// 토큰
-			var header = "Bearer " + client_id;
-			
-			console.log("헤더 요청 : ", header);
-
 			// 카페에 작성할 내용
-			var subject = encodeURI("네이버 카페 api Test node js");
-			var content = encodeURI("네이버 카페 api로 글을 카페에 글을 올려봅니다.");
+			var subject = encodeURI("안녕하세요, 세상을 구하는 붉은 인연, 피플입니다.");
+			var content = encodeURI("오늘도 즐거운 하루 되십시요...");
 
 			// 옵션
 			var cafe_options = {
 				url: cafe_api_url,
 				form: {'subject':subject, 'content':content},
-				headers: {'Authorization': header}
+				headers: {'Authorization': "Bearer " + body_data.access_token}
 			};
 
-			console.log('글쓰기 요청 헤더 : ', cafe_options);
+			// console.log('글쓰기 요청 헤더 : ', cafe_options);
 
 			// > POST /v1/cafe/{CAFE_ID}/menu/1/articles HTTP/1.1
 			// > Host: openapi.naver.com
@@ -74,11 +75,12 @@ app.get('/',function(req, res){
 			request.post(cafe_options, function (error, response, body) {
 				if (!error && response.statusCode == 200) {
 					console.log("포스팅 요청 완료! ", body);
+					res.end(body);
 				} else {
 					console.log('error');
-					if(response != null) {
-					res.status(response.statusCode).end();
-					console.log('error = ', response);
+						if(response != null) {
+						res.status(response.statusCode).end();
+						console.log(response.body);
 					}
 				}
 			});
@@ -101,8 +103,8 @@ console.log(puppeteer.executablePath());
 	const page = await browser.newPage();
 	await page.setViewport({ width: 1280, height: 1024, deviceScaleFactor: 0.8 });
 	await page.goto(api_url);
-	const email = 'eurobin4321';
-	const pw = 'hong1128.';
+	const email = 'contact_people';
+	const pw = 'bjk2355!';
 
 	await page.evaluate(
 		(em, pw) => {
